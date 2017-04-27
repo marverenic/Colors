9b4a034228b093c1f461c0263e47faf885384550
Andrew Bailey <marverenic@gmail.com>
2017-04-26 21:17:30 -0400

    Add color enum definitions

--- library/src/main/java/com/marverenic/colors/AccentColor.java
+++ library/src/main/java/com/marverenic/colors/AccentColor.java
@@ -7,6 +7,86 @@ import android.support.annotation.StyleRes;
 
 public enum AccentColor implements Parcelable {
 
+    RED_A100(R.style._ColorsTheme_Accent_Red_A100, R.color._color_lib_red_A100),
+    RED_A200(R.style._ColorsTheme_Accent_Red_A200, R.color._color_lib_red_A200),
+    RED_A400(R.style._ColorsTheme_Accent_Red_A400, R.color._color_lib_red_A400),
+    RED_A700(R.style._ColorsTheme_Accent_Red_A700, R.color._color_lib_red_A700),
+
+    PINK_A100(R.style._ColorsTheme_Accent_Pink_A100, R.color._color_lib_pink_A100),
+    PINK_A200(R.style._ColorsTheme_Accent_Pink_A200, R.color._color_lib_pink_A200),
+    PINK_A400(R.style._ColorsTheme_Accent_Pink_A400, R.color._color_lib_pink_A400),
+    PINK_A700(R.style._ColorsTheme_Accent_Pink_A700, R.color._color_lib_pink_A700),
+
+    PURPLE_A100(R.style._ColorsTheme_Accent_Purple_A100, R.color._color_lib_purple_A100),
+    PURPLE_A200(R.style._ColorsTheme_Accent_Purple_A200, R.color._color_lib_purple_A200),
+    PURPLE_A400(R.style._ColorsTheme_Accent_Purple_A400, R.color._color_lib_purple_A400),
+    PURPLE_A700(R.style._ColorsTheme_Accent_Purple_A700, R.color._color_lib_purple_A700),
+
+    DEEP_PURPLE_A100(R.style._ColorsTheme_Accent_DeepPurple_A100, R.color._color_lib_deep_purple_A100),
+    DEEP_PURPLE_A200(R.style._ColorsTheme_Accent_DeepPurple_A200, R.color._color_lib_deep_purple_A200),
+    DEEP_PURPLE_A400(R.style._ColorsTheme_Accent_DeepPurple_A400, R.color._color_lib_deep_purple_A400),
+    DEEP_PURPLE_A700(R.style._ColorsTheme_Accent_DeepPurple_A700, R.color._color_lib_deep_purple_A700),
+
+    INDIGO_A100(R.style._ColorsTheme_Accent_Indigo_A100, R.color._color_lib_indigo_A100),
+    INDIGO_A200(R.style._ColorsTheme_Accent_Indigo_A200, R.color._color_lib_indigo_A200),
+    INDIGO_A400(R.style._ColorsTheme_Accent_Indigo_A400, R.color._color_lib_indigo_A400),
+    INDIGO_A700(R.style._ColorsTheme_Accent_Indigo_A700, R.color._color_lib_indigo_A700),
+
+    BLUE_A100(R.style._ColorsTheme_Accent_Blue_A100, R.color._color_lib_blue_A100),
+    BLUE_A200(R.style._ColorsTheme_Accent_Blue_A200, R.color._color_lib_blue_A200),
+    BLUE_A400(R.style._ColorsTheme_Accent_Blue_A400, R.color._color_lib_blue_A400),
+    BLUE_A700(R.style._ColorsTheme_Accent_Blue_A700, R.color._color_lib_blue_A700),
+
+    LIGHT_BLUE_A100(R.style._ColorsTheme_Accent_LightBlue_A100, R.color._color_lib_light_blue_A100),
+    LIGHT_BLUE_A200(R.style._ColorsTheme_Accent_LightBlue_A200, R.color._color_lib_light_blue_A200),
+    LIGHT_BLUE_A400(R.style._ColorsTheme_Accent_LightBlue_A400, R.color._color_lib_light_blue_A400),
+    LIGHT_BLUE_A700(R.style._ColorsTheme_Accent_LightBlue_A700, R.color._color_lib_light_blue_A700),
+
+    CYAN_A100(R.style._ColorsTheme_Accent_Cyan_A100, R.color._color_lib_cyan_A100),
+    CYAN_A200(R.style._ColorsTheme_Accent_Cyan_A200, R.color._color_lib_cyan_A200),
+    CYAN_A400(R.style._ColorsTheme_Accent_Cyan_A400, R.color._color_lib_cyan_A400),
+    CYAN_A700(R.style._ColorsTheme_Accent_Cyan_A700, R.color._color_lib_cyan_A700),
+
+    TEAL_A100(R.style._ColorsTheme_Accent_Teal_A100, R.color._color_lib_teal_A100),
+    TEAL_A200(R.style._ColorsTheme_Accent_Teal_A200, R.color._color_lib_teal_A200),
+    TEAL_A400(R.style._ColorsTheme_Accent_Teal_A400, R.color._color_lib_teal_A400),
+    TEAL_A700(R.style._ColorsTheme_Accent_Teal_A700, R.color._color_lib_teal_A700),
+
+    GREEN_A100(R.style._ColorsTheme_Accent_Green_A100, R.color._color_lib_green_A100),
+    GREEN_A200(R.style._ColorsTheme_Accent_Green_A200, R.color._color_lib_green_A200),
+    GREEN_A400(R.style._ColorsTheme_Accent_Green_A400, R.color._color_lib_green_A400),
+    GREEN_A700(R.style._ColorsTheme_Accent_Green_A700, R.color._color_lib_green_A700),
+
+    LIGHT_GREEN_A100(R.style._ColorsTheme_Accent_LightGreen_A100, R.color._color_lib_light_green_A100),
+    LIGHT_GREEN_A200(R.style._ColorsTheme_Accent_LightGreen_A200, R.color._color_lib_light_green_A200),
+    LIGHT_GREEN_A400(R.style._ColorsTheme_Accent_LightGreen_A400, R.color._color_lib_light_green_A400),
+    LIGHT_GREEN_A700(R.style._ColorsTheme_Accent_LightGreen_A700, R.color._color_lib_light_green_A700),
+
+    LIME_A100(R.style._ColorsTheme_Accent_Lime_A100, R.color._color_lib_lime_A100),
+    LIME_A200(R.style._ColorsTheme_Accent_Lime_A200, R.color._color_lib_lime_A200),
+    LIME_A400(R.style._ColorsTheme_Accent_Lime_A400, R.color._color_lib_lime_A400),
+    LIME_A700(R.style._ColorsTheme_Accent_Lime_A700, R.color._color_lib_lime_A700),
+
+    YELLOW_A100(R.style._ColorsTheme_Accent_Yellow_A100, R.color._color_lib_yellow_A100),
+    YELLOW_A200(R.style._ColorsTheme_Accent_Yellow_A200, R.color._color_lib_yellow_A200),
+    YELLOW_A400(R.style._ColorsTheme_Accent_Yellow_A400, R.color._color_lib_yellow_A400),
+    YELLOW_A700(R.style._ColorsTheme_Accent_Yellow_A700, R.color._color_lib_yellow_A700),
+
+    AMBER_A100(R.style._ColorsTheme_Accent_Amber_A100, R.color._color_lib_amber_A100),
+    AMBER_A200(R.style._ColorsTheme_Accent_Amber_A200, R.color._color_lib_amber_A200),
+    AMBER_A400(R.style._ColorsTheme_Accent_Amber_A400, R.color._color_lib_amber_A400),
+    AMBER_A700(R.style._ColorsTheme_Accent_Amber_A700, R.color._color_lib_amber_A700),
+
+    ORANGE_A100(R.style._ColorsTheme_Accent_Orange_A100, R.color._color_lib_orange_A100),
+    ORANGE_A200(R.style._ColorsTheme_Accent_Orange_A200, R.color._color_lib_orange_A200),
+    ORANGE_A400(R.style._ColorsTheme_Accent_Orange_A400, R.color._color_lib_orange_A400),
+    ORANGE_A700(R.style._ColorsTheme_Accent_Orange_A700, R.color._color_lib_orange_A700),
+
+    DEEP_ORANGE_A100(R.style._ColorsTheme_Accent_DeepOrange_A100, R.color._color_lib_deep_orange_A100),
+    DEEP_ORANGE_A200(R.style._ColorsTheme_Accent_DeepOrange_A200, R.color._color_lib_deep_orange_A200),
+    DEEP_ORANGE_A400(R.style._ColorsTheme_Accent_DeepOrange_A400, R.color._color_lib_deep_orange_A400),
+    DEEP_ORANGE_A700(R.style._ColorsTheme_Accent_DeepOrange_A700, R.color._color_lib_deep_orange_A700),
+
     ;
 
     @StyleRes private int mTheme;
--- library/src/main/java/com/marverenic/colors/PrimaryColor.java
+++ library/src/main/java/com/marverenic/colors/PrimaryColor.java
@@ -7,6 +7,91 @@ import android.support.annotation.StyleRes;
 
 public enum PrimaryColor implements Parcelable {
 
+    RED_500(R.style._ColorsTheme_Primary_Red_500, R.color._color_lib_red_500, R.color._color_lib_red_700),
+    RED_600(R.style._ColorsTheme_Primary_Red_600, R.color._color_lib_red_600, R.color._color_lib_red_800),
+    RED_700(R.style._ColorsTheme_Primary_Red_700, R.color._color_lib_red_700, R.color._color_lib_red_900),
+    RED_800(R.style._ColorsTheme_Primary_Red_800, R.color._color_lib_red_500, R.color._color_lib_red_900),
+
+    PINK_500(R.style._ColorsTheme_Primary_Pink_500, R.color._color_lib_pink_500, R.color._color_lib_pink_700),
+    PINK_600(R.style._ColorsTheme_Primary_Pink_600, R.color._color_lib_pink_600, R.color._color_lib_pink_800),
+    PINK_700(R.style._ColorsTheme_Primary_Pink_700, R.color._color_lib_pink_700, R.color._color_lib_pink_900),
+    PINK_800(R.style._ColorsTheme_Primary_Pink_800, R.color._color_lib_pink_500, R.color._color_lib_pink_900),
+
+    PURPLE_500(R.style._ColorsTheme_Primary_Purple_500, R.color._color_lib_purple_500, R.color._color_lib_purple_700),
+    PURPLE_600(R.style._ColorsTheme_Primary_Purple_600, R.color._color_lib_purple_600, R.color._color_lib_purple_800),
+    PURPLE_700(R.style._ColorsTheme_Primary_Purple_700, R.color._color_lib_purple_700, R.color._color_lib_purple_900),
+    PURPLE_800(R.style._ColorsTheme_Primary_Purple_800, R.color._color_lib_purple_500, R.color._color_lib_purple_900),
+
+    DEEP_PURPLE_500(R.style._ColorsTheme_Primary_DeepPurple_500, R.color._color_lib_deep_purple_500, R.color._color_lib_deep_purple_700),
+    DEEP_PURPLE_600(R.style._ColorsTheme_Primary_DeepPurple_600, R.color._color_lib_deep_purple_600, R.color._color_lib_deep_purple_800),
+    DEEP_PURPLE_700(R.style._ColorsTheme_Primary_DeepPurple_700, R.color._color_lib_deep_purple_700, R.color._color_lib_deep_purple_900),
+    DEEP_PURPLE_800(R.style._ColorsTheme_Primary_DeepPurple_800, R.color._color_lib_deep_purple_500, R.color._color_lib_deep_purple_900),
+
+    INDIGO_500(R.style._ColorsTheme_Primary_Indigo_500, R.color._color_lib_indigo_500, R.color._color_lib_indigo_700),
+    INDIGO_600(R.style._ColorsTheme_Primary_Indigo_600, R.color._color_lib_indigo_600, R.color._color_lib_indigo_800),
+    INDIGO_700(R.style._ColorsTheme_Primary_Indigo_700, R.color._color_lib_indigo_700, R.color._color_lib_indigo_900),
+    INDIGO_800(R.style._ColorsTheme_Primary_Indigo_800, R.color._color_lib_indigo_500, R.color._color_lib_indigo_900),
+
+    BLUE_500(R.style._ColorsTheme_Primary_Blue_500, R.color._color_lib_blue_500, R.color._color_lib_blue_700),
+    BLUE_600(R.style._ColorsTheme_Primary_Blue_600, R.color._color_lib_blue_600, R.color._color_lib_blue_800),
+    BLUE_700(R.style._ColorsTheme_Primary_Blue_700, R.color._color_lib_blue_700, R.color._color_lib_blue_900),
+    BLUE_800(R.style._ColorsTheme_Primary_Blue_800, R.color._color_lib_blue_500, R.color._color_lib_blue_900),
+
+    LIGHT_BLUE_500(R.style._ColorsTheme_Primary_LightBlue_500, R.color._color_lib_light_blue_500, R.color._color_lib_light_blue_700),
+    LIGHT_BLUE_600(R.style._ColorsTheme_Primary_LightBlue_600, R.color._color_lib_light_blue_600, R.color._color_lib_light_blue_800),
+    LIGHT_BLUE_700(R.style._ColorsTheme_Primary_LightBlue_700, R.color._color_lib_light_blue_700, R.color._color_lib_light_blue_900),
+    LIGHT_BLUE_800(R.style._ColorsTheme_Primary_LightBlue_800, R.color._color_lib_light_blue_500, R.color._color_lib_light_blue_900),
+
+    CYAN_500(R.style._ColorsTheme_Primary_Cyan_500, R.color._color_lib_cyan_500, R.color._color_lib_cyan_700),
+    CYAN_600(R.style._ColorsTheme_Primary_Cyan_600, R.color._color_lib_cyan_600, R.color._color_lib_cyan_800),
+    CYAN_700(R.style._ColorsTheme_Primary_Cyan_700, R.color._color_lib_cyan_700, R.color._color_lib_cyan_900),
+    CYAN_800(R.style._ColorsTheme_Primary_Cyan_800, R.color._color_lib_cyan_500, R.color._color_lib_cyan_900),
+
+    TEAL_500(R.style._ColorsTheme_Primary_Teal_500, R.color._color_lib_teal_500, R.color._color_lib_teal_700),
+    TEAL_600(R.style._ColorsTheme_Primary_Teal_600, R.color._color_lib_teal_600, R.color._color_lib_teal_800),
+    TEAL_700(R.style._ColorsTheme_Primary_Teal_700, R.color._color_lib_teal_700, R.color._color_lib_teal_900),
+    TEAL_800(R.style._ColorsTheme_Primary_Teal_800, R.color._color_lib_teal_500, R.color._color_lib_teal_900),
+
+    GREEN_500(R.style._ColorsTheme_Primary_Green_500, R.color._color_lib_green_500, R.color._color_lib_green_700),
+    GREEN_600(R.style._ColorsTheme_Primary_Green_600, R.color._color_lib_green_600, R.color._color_lib_green_800),
+    GREEN_700(R.style._ColorsTheme_Primary_Green_700, R.color._color_lib_green_700, R.color._color_lib_green_900),
+    GREEN_800(R.style._ColorsTheme_Primary_Green_800, R.color._color_lib_green_500, R.color._color_lib_green_900),
+
+    LIGHT_GREEN_500(R.style._ColorsTheme_Primary_LightGreen_500, R.color._color_lib_light_green_500, R.color._color_lib_light_green_700),
+    LIGHT_GREEN_600(R.style._ColorsTheme_Primary_LightGreen_600, R.color._color_lib_light_green_600, R.color._color_lib_light_green_800),
+    LIGHT_GREEN_700(R.style._ColorsTheme_Primary_LightGreen_700, R.color._color_lib_light_green_700, R.color._color_lib_light_green_900),
+    LIGHT_GREEN_800(R.style._ColorsTheme_Primary_LightGreen_800, R.color._color_lib_light_green_500, R.color._color_lib_light_green_900),
+
+    LIME_500(R.style._ColorsTheme_Primary_Lime_500, R.color._color_lib_lime_500, R.color._color_lib_lime_700),
+    LIME_600(R.style._ColorsTheme_Primary_Lime_600, R.color._color_lib_lime_600, R.color._color_lib_lime_800),
+    LIME_700(R.style._ColorsTheme_Primary_Lime_700, R.color._color_lib_lime_700, R.color._color_lib_lime_900),
+    LIME_800(R.style._ColorsTheme_Primary_Lime_800, R.color._color_lib_lime_500, R.color._color_lib_lime_900),
+
+    YELLOW_500(R.style._ColorsTheme_Primary_Yellow_500, R.color._color_lib_yellow_500, R.color._color_lib_yellow_700),
+    YELLOW_600(R.style._ColorsTheme_Primary_Yellow_600, R.color._color_lib_yellow_600, R.color._color_lib_yellow_800),
+    YELLOW_700(R.style._ColorsTheme_Primary_Yellow_700, R.color._color_lib_yellow_700, R.color._color_lib_yellow_900),
+    YELLOW_800(R.style._ColorsTheme_Primary_Yellow_800, R.color._color_lib_yellow_500, R.color._color_lib_yellow_900),
+
+    AMBER_500(R.style._ColorsTheme_Primary_Amber_500, R.color._color_lib_amber_500, R.color._color_lib_amber_700),
+    AMBER_600(R.style._ColorsTheme_Primary_Amber_600, R.color._color_lib_amber_600, R.color._color_lib_amber_800),
+    AMBER_700(R.style._ColorsTheme_Primary_Amber_700, R.color._color_lib_amber_700, R.color._color_lib_amber_900),
+    AMBER_800(R.style._ColorsTheme_Primary_Amber_800, R.color._color_lib_amber_500, R.color._color_lib_amber_900),
+
+    ORANGE_500(R.style._ColorsTheme_Primary_Orange_500, R.color._color_lib_orange_500, R.color._color_lib_orange_700),
+    ORANGE_600(R.style._ColorsTheme_Primary_Orange_600, R.color._color_lib_orange_600, R.color._color_lib_orange_800),
+    ORANGE_700(R.style._ColorsTheme_Primary_Orange_700, R.color._color_lib_orange_700, R.color._color_lib_orange_900),
+    ORANGE_800(R.style._ColorsTheme_Primary_Orange_800, R.color._color_lib_orange_500, R.color._color_lib_orange_900),
+
+    DEEP_ORANGE_500(R.style._ColorsTheme_Primary_DeepOrange_500, R.color._color_lib_deep_orange_500, R.color._color_lib_deep_orange_700),
+    DEEP_ORANGE_600(R.style._ColorsTheme_Primary_DeepOrange_600, R.color._color_lib_deep_orange_600, R.color._color_lib_deep_orange_800),
+    DEEP_ORANGE_700(R.style._ColorsTheme_Primary_DeepOrange_700, R.color._color_lib_deep_orange_700, R.color._color_lib_deep_orange_900),
+    DEEP_ORANGE_800(R.style._ColorsTheme_Primary_DeepOrange_800, R.color._color_lib_deep_orange_500, R.color._color_lib_deep_orange_900),
+
+    BROWN_500(R.style._ColorsTheme_Primary_Brown_500, R.color._color_lib_brown_500, R.color._color_lib_brown_700),
+    BROWN_600(R.style._ColorsTheme_Primary_Brown_600, R.color._color_lib_brown_600, R.color._color_lib_brown_800),
+    BROWN_700(R.style._ColorsTheme_Primary_Brown_700, R.color._color_lib_brown_700, R.color._color_lib_brown_900),
+    BROWN_800(R.style._ColorsTheme_Primary_Brown_800, R.color._color_lib_brown_500, R.color._color_lib_brown_900),
+
     ;
 
     @StyleRes private int mTheme;
