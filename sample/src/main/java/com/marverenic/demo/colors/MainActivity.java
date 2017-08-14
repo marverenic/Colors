3b3b6b35f3d9c9a43e9e7ca57243bee8781be572
Andrew Bailey <marverenic@gmail.com>
2017-08-14 17:30:36 -0400

    Add Night Mode options to sample app

--- sample/src/main/java/com/marverenic/demo/colors/MainActivity.java
+++ sample/src/main/java/com/marverenic/demo/colors/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Spinner;
 import com.marverenic.colors.AccentColor;
 import com.marverenic.colors.ColorTheme;
 import com.marverenic.colors.Colors;
+import com.marverenic.colors.NightMode;
 import com.marverenic.colors.PrimaryColor;
 import com.marverenic.colors.activity.ColorsAppCompatActivity;
 
@@ -17,6 +18,7 @@ public class MainActivity extends ColorsAppCompatActivity {
 
     private Spinner mPrimarySpinner;
     private Spinner mAccentSpinner;
+    private Spinner mNightModeSpinner;
     private Button mRandomButton;
 
     @Override
@@ -26,10 +28,12 @@ public class MainActivity extends ColorsAppCompatActivity {
 
         mPrimarySpinner = (Spinner) findViewById(R.id.spinnerPrimary);
         mAccentSpinner = (Spinner) findViewById(R.id.spinnerAccent);
+        mNightModeSpinner = (Spinner) findViewById(R.id.spinnerNightMode);
         mRandomButton = (Button) findViewById(R.id.buttonRandom);
 
         mPrimarySpinner.setAdapter(new EnumAdapter<>(PrimaryColor.values()));
         mAccentSpinner.setAdapter(new EnumAdapter<>(AccentColor.values()));
+        mNightModeSpinner.setAdapter(new EnumAdapter<>(NightMode.values()));
 
         setupCallbacks();
     }
@@ -40,9 +44,11 @@ public class MainActivity extends ColorsAppCompatActivity {
 
         int primary = indexOf(PrimaryColor.values(), Colors.getTheme().getPrimaryColor());
         int accent = indexOf(AccentColor.values(), Colors.getTheme().getAccentColor());
+        int nightMode = indexOf(NightMode.values(), Colors.getTheme().getNightMode());
 
         mPrimarySpinner.setSelection(primary);
         mAccentSpinner.setSelection(accent);
+        mNightModeSpinner.setSelection(nightMode);
     }
 
     private static <T> int indexOf(T[] arr, T val) {
@@ -89,6 +95,24 @@ public class MainActivity extends ColorsAppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {}
         });
 
+        mNightModeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                NightMode nightMode = NightMode.values()[position];
+                ColorTheme currentTheme = Colors.getTheme();
+
+                if (nightMode != currentTheme.getNightMode()) {
+                    ColorTheme theme = new ColorTheme(currentTheme.getPrimaryColor(),
+                            currentTheme.getAccentColor(), nightMode);
+                    Colors.setTheme(theme);
+                    recreate();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
         mRandomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,11 +124,14 @@ public class MainActivity extends ColorsAppCompatActivity {
     private void randomizeTheme() {
         PrimaryColor[] primaryColors = PrimaryColor.values();
         AccentColor[] accentColors = AccentColor.values();
+        NightMode[] nightModes = NightMode.values();
 
         int whichPrimary = (int) (Math.random() * primaryColors.length);
         int whichAccent = (int) (Math.random() * accentColors.length);
+        int whichNightMode = (int) (Math.random() * nightModes.length);
 
-        ColorTheme random = new ColorTheme(primaryColors[whichPrimary], accentColors[whichAccent]);
+        ColorTheme random = new ColorTheme(primaryColors[whichPrimary], accentColors[whichAccent],
+                nightModes[whichNightMode]);
         Colors.setTheme(random);
         recreate();
     }
